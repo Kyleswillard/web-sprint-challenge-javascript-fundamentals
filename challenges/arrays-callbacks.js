361e61dadf65dc1e76b3f16dfd35f80547ad2a00
Kyle Willard <68755034+Kyleswillard@users.noreply.github.com>
2020-10-09 18:45:19 -0700

    MVP MET, 1 Stretch Complete, Second Stretch Attempted

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,7 +21,12 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
-console.log(displayNames);
+zooAnimals.forEach((e) => {
+const names =`${e.animal_name}, ${e.scientific_name}`
+return displayNames.push(names)
+
+})
+// console.log(displayNames);
 
 /* Request 2: .map()
 
@@ -29,23 +34,27 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
-console.log(lowCaseAnimalNames);
+const lowCaseAnimalNames = zooAnimals.map((name) => {
+  return name.animal_name.toLowerCase();
+})
+// console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals
-console.log(lowPopulationAnimals);
+const lowPopulationAnimals = zooAnimals.filter((e) => e.population < 5)
+ console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
+let populationTotal = zooAnimals.reduce((acc, cur) => {
+  return acc + cur.population;
+  }, 0);
 console.log(populationTotal);
 
 
@@ -57,26 +66,28 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+consume = (a, b, cb) => cb(a, b);  
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+add = (num1, num2)  =>  num1 + num2;
+multiply = (num1, num2) =>  num1 * num2;
+greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
 
 /*
 
-Stretch: If you haven't already, convert your array method callbacks into arrow functions.
+[COMPLETED!] Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
 */
 
--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,18 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation:  nestedFunction can access the internal variable because it is within block scope. Anything outside of the nested function it can grab but not vice versa. So it can reach out to internal, but if we put the const inside of the nested function is would not be accessable to something above the nested function. TL;DR: You can reach out, but not in.
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+summation = (num) => {
+  const count = []
+    for (let i = 0; i <= num; i++) {
+      count.push(i)    
+  }
+  const total = count.reduce((acc, cur) => acc +cur);
+  return total;
+  }
+  summation(10)
